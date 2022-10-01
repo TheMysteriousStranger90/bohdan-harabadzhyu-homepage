efa8e13b5014ad2638851f179229ecba0c730bd8
Bohdan Harabadzhyu <bohdan_harabadzhyu@outlook.com>
2022-10-01 10:33:37 +0300

    Init...

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { type AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from '../components/BasicTheme';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+import { extendTheme } from '@chakra-ui/react'
 
-export default MyApp
+const App = ({ Component, pageProps, router }: AppProps) => {
+  return (
+      <ChakraProvider theme={theme}>
+          <Component {...pageProps} />
+      </ChakraProvider>
+  );
+};
+
+export default App;
+
